943c713cd93ca8a7c40ec1410336abf349ff9a22
sunwilston <sunwilston@gmail.com>
2011-11-28 09:03:05 +0000

    disable pdb

--- .emacs.d/config/a.05.python.el
+++ .emacs.d/config/a.05.python.el
@@ -3,6 +3,6 @@
              (hs-minor-mode) ;打开可以折叠的模式
              (local-set-key "\r" '(lambda() (interactive) (insert "\n") (python-indent-line))))) ;回车后自动缩进
 
-(add-to-list 'load-path "~/.emacs.d/site-lisp/pydb")
-(require 'pydb)
-(autoload 'pydb "pydb" "Python Debugger mode via GUD and pydb" t)
+;;(add-to-list 'load-path "~/.emacs.d/site-lisp/pydb") 
+;;(require 'pydb)
+;;(autoload 'pydb "pydb" "Python Debugger mode via GUD and pydb" t)
